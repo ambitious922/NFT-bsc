4ec32cc9ed9451462392ad272a421a27a81ba931
Serniorwebdev123 <ghost15824@gmail.com>
2021-10-04 07:17:22 -0700

    NFT-bsc-golf

--- src/components/header.js
+++ src/components/header.js
@@ -89,11 +89,32 @@ function Header() {
       }
       <Modal show={openModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title><p className="Tanker wallet-connect-title">WALLET CONNECTION</p></Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          <div className="text-center">
+            <Button variant="secondary">
+              <div className="metamask text-center">
+                <img className="metamask-img" src="./assets/images/w1.svg"/>
+                <p className="metamask-title Tanker p-1">MetaMask</p>
+                <p className="metamask-txt Tanker pb-1">Connect to your MetaMask Wallet</p>
+              </div>
+            </Button>
+          </div> 
+        </Modal.Body>
+        <Modal.Body>
+          <div className="text-center">
+            <Button variant="secondary">
+              <div className="walletconnect text-center">
+                <img className="walletconnect-img" src="./assets/images/w2.svg"/>
+                <p className="walletconnect-title Tanker p-1">MetaMask</p>
+                <p className="walletconnect-txt Tanker pb-1">Connect to your MetaMask Wallet</p>
+              </div>
+            </Button>
+          </div>  
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => setOpenModal(!openModal)}>Close</Button>
+          <Button onClick={() => setOpenModal(!openModal)}><p className="walletconnect-colse Tanker pb-1">Close</p></Button>
         </Modal.Footer>
       </Modal>
     </div>
